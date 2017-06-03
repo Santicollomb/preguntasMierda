c7107293235585782c516409246580df2a5b9342
santicollomb <santicollomb@hotmail.com>
2017-06-03 17:28:27 -0300

    Juego con Before y After3

--- src/components/tap-reveal/tap-reveal.ts
+++ src/components/tap-reveal/tap-reveal.ts
@@ -7,12 +7,8 @@ import { Component } from '@angular/core';
 export class TapRevealComponent {
 
   toggled: boolean = false;
-
+isClassVisible: false;
   constructor() {
-
-  }
-
-  mostrarEstadisticas() {
 
   }
 
